29198e1945c37c67441cd1bde886bb0e1b43be63
victoriaotm <victoriaotmahova@gmail.com>
2018-03-11 01:46:58 +0300

    Profile + leader board fix[2]

--- public/js/application.js
+++ public/js/application.js
@@ -6,19 +6,22 @@ import renderDOM from './components/render/render.js';
 import SignupSection from './components/pages/signupSection.js';
 import PlaySection from './components/pages/PlaySection.js';
 import LeaderBoard from './components/pages/leaderBoard.js';
+import Profile from './components/pages/Profile.js';
 
 const menuSection = new Menu();
 const loginSection = new LoginSection();
 const signUpSection = new SignupSection();
 const playSection = new PlaySection();
 const leaderBoard = new LeaderBoard();
+const profileSection = new Profile();
 
 export const Sections = {
   'Menu': menuSection,
   'Login': loginSection,
   'Signup': signUpSection,
   'Play': playSection,
-  'LeaderBoard': leaderBoard
+  'LeaderBoard': leaderBoard,
+  'Profile': profileSection
 };
 
 renderDOM(menuSection.render(), document.getElementById('root'));
