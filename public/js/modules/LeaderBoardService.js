3cb2396b5f05c6d2fef05b66f9b29c7c26450a03
victoriaotm <victoriaotmahova@gmail.com>
2018-03-11 00:27:43 +0300

    Leader Board (without pagination)

---
+++ public/js/modules/LeaderBoardService.js
@@ -0,0 +1,37 @@
+import http from './FetchService.js';
+import config from './config.js';
+import UserService from './UserService.js';
+import Logger from '../utils/logger.js';
+
+export default class LeaderBoardService {
+
+  /**
+   *
+   * @param {Number} page
+   * @returns {Promise<Response> | *}
+   */
+  static getLeaderBoard(page) {
+    const path = config.APIMethods.leaderBoard + `/${page}`;
+    Logger.log(path);
+    this.LBJson = http.post(path, undefined)
+      .then((response) => {
+        switch (response.status) {
+          case 200:
+            return response.json();
+          case 400:
+            return;
+          default:
+            throw {status: 'Error', message: 'Unexpected error'};
+        }
+      })
+      .then((json) => {
+        if (json) {
+          return json;
+        }
+      })
+      .catch(UserService.errorTransformer);
+
+    return this.LBJson;
+  }
+
+}
