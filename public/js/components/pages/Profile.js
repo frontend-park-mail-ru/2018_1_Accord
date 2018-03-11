6c42d8120233355cac32c8f9cbddf09b6620bc1d
victoriaotm <victoriaotmahova@gmail.com>
2018-03-12 00:08:07 +0300

    Profile updated

--- public/js/components/pages/Profile.js
+++ public/js/components/pages/Profile.js
@@ -1,6 +1,10 @@
 import Section from './section.js';
 import TextField from '../blocks/textField.js';
 import BackButton from '../blocks/backButton.js';
+import UserService from '../../modules/UserService.js';
+import Button from '../blocks/button.js';
+
+const userDataTemplate = window.fest['js/components/pages/Profile.tmpl'];
 
 export default class Profile extends Section {
   constructor() {
@@ -16,6 +20,18 @@ export default class Profile extends Section {
     this.profile.appendChild(this.title.render());
 
     this.backButton.onClick();
+
+    UserService.getUser()
+      .then((user) => {
+        if (user) {
+          this.userDataText = document.createElement('div');
+          this.userDataText.innerHTML = userDataTemplate(user.getProfileData());
+
+          this.updateButton = new Button('button', 'Update');
+          this.profile.appendChild(this.userDataText);
+          this.profile.appendChild(this.updateButton.render());
+        }
+      });
 
     return this.profile;
   }
--- public/js/modules/User.js
+++ public/js/modules/User.js
@@ -1,7 +1,15 @@
 export default class User {
   constructor(object) {
+    this.id = object.id;
     this.email = object.email;
     this.username = object.nickname;
     this.rating = object.rating;
+  }
+
+  getProfileData() {
+    return {
+      'username': this.username,
+      'rating': this.rating
+    };
   }
 }
