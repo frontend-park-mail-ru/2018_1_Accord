8085a5a262821440ef2d349a56bfc73efd9d85ee
victoriaotm <victoriaotmahova@gmail.com>
2018-04-08 18:42:34 +0300

    directories moved

---
+++ src/main/application.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import Router from './modules/router.js';
+import {selector} from './config/selector.js';
+import MenuView from './views/pages/MainMenuView/mainMenuView.js';
+import LoginView from './views/pages/LoginView/loginView.js';
+import HelpView from './views/pages/HelpView/helpView.js';
+import LeaderBoardView from './views/pages/LeaderBoard/leaderBoardView.js';
+import SignUpView from './views/pages/SignUpView/signUpView.js';
+import GameView from './views/pages/GameView/gameView.js';
+//import ProfileView from './views/pages/ProfileView/profileView.main';
+import {pagePaths} from './config/pagePaths.js';
+
+
+export default class Application {
+  constructor() {
+    this.root = document.getElementById(selector.ROOT);
+  }
+
+  run() {
+    new Router(this.root)
+      .add(pagePaths.START_PATH, MenuView)
+      .add(pagePaths.LOGIN_PATH, LoginView)
+      .add(pagePaths.HELP_PATH, HelpView)
+      .add(pagePaths.LEADER_BOARD_PATH, LeaderBoardView)
+      .add(pagePaths.SIGN_UP_PATH, SignUpView)
+      .add(pagePaths.GAME_PATH, GameView)
+      //.add(pagePaths.PROFILE_PATH, ProfileView)
+      .start();
+  }
+}
---
+++ src/main/components/blocks/buttons/logoutButton.js
@@ -0,0 +1,45 @@
+import userService from '../../../services/UserService.js';
+import Router from '../../../modules/router.js';
+import Logger from '../../../utils/logger.js';
+import {selector} from '../../../config/selector.js';
+import {pagePaths} from '../../../config/pagePaths.js';
+
+export default class LogoutButton {
+
+  /**
+   * @param {HTMLElement} element
+   * @param {String} className
+   */
+  constructor(element, className) {
+    this.errorField = element.querySelector(selector.MAIN_ERROR);
+    this.errorField.style.display = 'block';
+
+    this.logoutButton = element.querySelector(className);
+    this.logoutButton.style.display = 'block';
+    this.logoutButton.innerText = 'Logout';
+    this._onClick();
+  }
+
+  render() {
+    return this.logoutButton;
+  }
+
+  /**
+   * @var {Error} error
+   * @private
+   */
+  _onClick() {
+    this.logoutButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      userService.logout()
+        .then(() => {
+          history.pushState(null, '', pagePaths.START_PATH);
+          Logger.log('Logout successfully');
+        })
+        .catch((error) => {
+          this.errorField.innerText = error.message;
+          Logger.error(error);
+        });
+    });
+  }
+}
