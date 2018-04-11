7ab0a9f06e0ae0e2f3fb075524d5925b6629f166
msamokhina <samokhina.marina.97@gmail.com>
2018-04-12 01:38:46 +0300

    Service Worker and errors fixed

--- server/index.js
+++ server/index.js
@@ -12,12 +12,12 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '..', 'src')));
 app.use(body.json());
-app.use('/login', express.static('src'));
-app.use('/signup', express.static('src'));
-app.use('/help', express.static('src'));
-app.use('/game', express.static('src'));
-//app.use('/profile', express.static('src'));
-app.use('/leaderboard', express.static('src'));
+app.use('/login/', express.static('src'));
+app.use('/signup/', express.static('src'));
+app.use('/help/', express.static('src'));
+app.use('/game/', express.static('src'));
+//app.use('/profile/', express.static('src'));
+app.use('/leaderboard/', express.static('src'));
 
 const port = process.env.PORT || 8000;
 
---
+++ src/sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'donuts-wars_sw';
+const cacheUrls = [
+  '/swResponse.png'
+];
+
+this.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(cacheUrls);
+      })
+      .catch((err) => {
+        console.error('smth went wrong with caches.open: ', err);
+      })
+  );
+});
+
+
+this.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        return cachedResponse || fetch(event.request).then((serverResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, serverResponse.clone());
+            return serverResponse;
+          });
+        });
+      })
+      .catch(() => {
+        return caches.match('/swResponse.png');
+      })
+  );
+});
