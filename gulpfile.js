538d9c15be27cd8997eef2eff20c30360aa823df
msamokhina <samokhina.marina.97@gmail.com>
2018-05-04 14:49:48 +0300

    autoprefixer added

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,20 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-scss
+    .pipe(gulp.dest('src/css'));
+});
+
+
+gulp.task('autoprefixer', function() {
+  return gulp.src('src/css/**/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('src/css'));
 });
