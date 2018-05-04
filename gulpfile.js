ac15f6713e1bfa63134fa49cbcfabd038f00e77c
msamokhina <samokhina.marina.97@gmail.com>
2018-05-04 15:28:42 +0300

    sass + autoprefixer = style

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,12 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('sass', function(){
+gulp.task('style', function(){
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-scss
-    .pipe(gulp.dest('src/css'));
-});
-
-
-gulp.task('autoprefixer', function() {
-  return gulp.src('src/css/**/*.css')
     .pipe(autoprefixer({
       browsers: ['last 2 versions'],
-      cascade: false
+      cascade: true
     }))
     .pipe(gulp.dest('src/css'));
 });
